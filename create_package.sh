1f0e8625ceb29b6fc080f87df96393b2961da7a5
Dylan Delaporte <dylan.delaporte06@gmail.com>
2020-03-11 15:58:17 +0900

    fix: package script

--- create_package.sh
+++ create_package.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-PACKAGE_NAME="package"
+PACKAGE_NAME="object"
 
 SERVICE_DIR="$PACKAGE_NAME/etc/systemd/system"
 BIN_DIR="$PACKAGE_NAME/usr/local/bin/iot-object-workshop-monitoring"
 
 mkdir -p $PACKAGE_NAME
 
-cp DEBIAN/ $PACKAGE_NAME
+cp -r DEBIAN/ $PACKAGE_NAME
+chmod -R 775 $PACKAGE_NAME/DEBIAN
 
 mkdir -p $SERVICE_DIR
 cp service/* $SERVICE_DIR
 
 mkdir -p $BIN_DIR
-cp backup/ $BIN_DIR
-cp config/ $BIN_DIR
-cp record/ $BIN_DIR
+cp -r backup/ $BIN_DIR
+cp -r config/ $BIN_DIR
+cp -r record/ $BIN_DIR
 cp launcher.py $BIN_DIR
 cp hard-config.yml $BIN_DIR
 cp soft-config.yml $BIN_DIR
